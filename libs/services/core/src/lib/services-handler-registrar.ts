805c98739a9b80c3847d030d214e629bc25acfc4
Chua Siang Poh <siangpoh.chua@contour.network>
2022-03-10 13:51:23 +0800

    add handler core

--- libs/services/core/src/lib/services-handler-registrar.ts
+++ libs/services/core/src/lib/services-handler-registrar.ts
@@ -8,4 +8,17 @@ export enum HttpMethod {
 
 export type Handler<E, R, C = unknown> = (event: E, context: C) => Promise<R>;
 
-export type Registry<E, R, C> = { [P in HttpMethod]: Handler<E, R, C>[] };
+export type Registry<E, R, C> = Partial<{
+  [P in HttpMethod]: { [name: string]: Handler<E, R, C> };
+}>;
+
+/** @todo */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const registrar: Registry<any, any, any> = {};
+
+export const register = <E, R, C>(
+  method: HttpMethod,
+  handler: Handler<E, R, C>
+) => {
+  registrar[method] = { ...registrar[method], [handler.name]: handler };
+};
