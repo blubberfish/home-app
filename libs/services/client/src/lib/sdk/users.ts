1907e78e24ccc050a0c64ba5705c65bfb545971c
Chua Siang Poh <siangpoh.chua@contour.network>
2022-03-11 09:12:20 +0800

    fix login api

--- libs/services/client/src/lib/sdk/users.ts
+++ libs/services/client/src/lib/sdk/users.ts
@@ -1,34 +1,35 @@
-import { BaseUrl, HttpMethod, User } from '@blubberfish/types'
-import { createApi } from './utils'
+import { BaseUrl, HttpMethod, User } from '@blubberfish/types';
+import { createApi } from './utils';
 
-export const login = createApi<{ username: string, password: string }, User>({
-    method: HttpMethod.POST,
-    url: `${BaseUrl.REST}/login`,
-    builders: {
-        response: Promise.resolve
-    }
-})
+export const login = createApi<{ username: string; password: string }, User>({
+  method: HttpMethod.POST,
+  url: `${BaseUrl.REST}/login`,
+  builders: {
+    body: (input) => JSON.stringify(input),
+    response: Promise.resolve,
+  },
+});
 
 export const listUser = createApi<void, User[]>({
-    method: HttpMethod.POST,
-    url: `${BaseUrl.REST}/users`,
-    builders: {
-        response: Promise.resolve
-    }
-})
+  method: HttpMethod.POST,
+  url: `${BaseUrl.REST}/users`,
+  builders: {
+    response: Promise.resolve,
+  },
+});
 
 export const createUser = createApi<User, void>({
-    method: HttpMethod.POST,
-    url: `${BaseUrl.REST}/user`,
-    builders: {
-        body: (input) => JSON.stringify(input)
-    }
-})
+  method: HttpMethod.POST,
+  url: `${BaseUrl.REST}/user`,
+  builders: {
+    body: (input) => JSON.stringify(input),
+  },
+});
 
 export const deleteUser = createApi<string, void>({
-    method: HttpMethod.POST,
-    url: `${BaseUrl.REST}/user`,
-    builders: {
-        url: (input, base) => `${base}/${input}`
-    }
-})
+  method: HttpMethod.POST,
+  url: `${BaseUrl.REST}/user`,
+  builders: {
+    url: (input, base) => `${base}/${input}`,
+  },
+});
