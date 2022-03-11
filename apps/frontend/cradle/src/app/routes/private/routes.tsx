97dedab0e8e0484f7d1e7ff6e03812af459627ea
Chua Siang Poh <siangpoh.chua@contour.network>
2022-03-11 18:18:37 +0800

    update routes

--- apps/frontend/cradle/src/app/routes/private/routes.tsx
+++ apps/frontend/cradle/src/app/routes/private/routes.tsx
@@ -1,8 +1,11 @@
 import { Switch, Redirect, Route } from 'react-router-dom';
 
+const empty = () => <div />;
+
 export default () => {
   return (
     <Switch>
+      <Route path="/" component={empty} />
       <Route>
         <Redirect to="/" />
       </Route>
