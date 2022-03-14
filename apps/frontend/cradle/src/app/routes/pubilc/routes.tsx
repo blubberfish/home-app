dae547555b442b68007f3a355234878aba159f0b
Chua Siang Poh <siangpoh.chua@contour.network>
2022-03-14 14:07:46 +0800

    change landing page

--- apps/frontend/cradle/src/app/routes/pubilc/routes.tsx
+++ apps/frontend/cradle/src/app/routes/pubilc/routes.tsx
@@ -3,13 +3,13 @@ import { Switch, Redirect, Route } from 'react-router-dom';
 import { FrontEndPublicPath } from '@blubberfish/frontend/pages/routes';
 
 const LandingPage = lazy(() => import('@blubberfish/frontend/pages/landing'));
-const LoginPage = lazy(() => import('@blubberfish/frontend/pages/login'));
+// const LoginPage = lazy(() => import('@blubberfish/frontend/pages/login'));
 
 export default () => {
   return (
     <Switch>
       <Route path={FrontEndPublicPath.Root} component={LandingPage} exact />
-      <Route path={FrontEndPublicPath.Login} component={LoginPage} />
+      <Route path={FrontEndPublicPath.Login} component={() => null} />
       <Route>
         <Redirect to={FrontEndPublicPath.Root} />
       </Route>
--- libs/frontend/components/src/index.ts
+++ libs/frontend/components/src/index.ts
@@ -1,2 +1,3 @@
 export * from './lib/box';
 export * from './lib/global-style';
+export * from './types';
