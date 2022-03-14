0781abad53b2e59bd390e80d89edec9bb6246e47
Chua Siang Poh <siangpoh.chua@contour.network>
2022-03-14 12:27:08 +0800

    add global theme style

--- libs/frontend/components/src/lib/global.tsx
+++ libs/frontend/components/src/lib/global.tsx
@@ -1,7 +1,25 @@
-import { createGlobalStyle } from 'styled-components';
-import { typography } from './theme/helpers';
-import { TypographyProps } from '../types';
+import { PropsWithChildren } from 'react';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import { fontFamily, fontSize } from './theme/helpers';
+import { FontFamilyProps, FontSizeProps, Theme } from '../types';
 
-const Style = createGlobalStyle<TypographyProps>`
-  ${typography}
+const Style = createGlobalStyle<FontFamilyProps & FontSizeProps>`
+  ${fontFamily}
+  ${fontSize}
 `;
+
+export const Global = <T extends Theme = Theme>({
+  children,
+  theme,
+  ...rest
+}: PropsWithChildren<
+  FontFamilyProps &
+    FontSizeProps & {
+      theme?: T;
+    }
+>) => (
+  <ThemeProvider theme={theme}>
+    <Style {...rest} />
+    {children}
+  </ThemeProvider>
+);
