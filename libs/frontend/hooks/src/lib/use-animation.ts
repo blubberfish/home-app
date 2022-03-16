935188eac891718048cc595b4c355581986b1665
poh <siangpoh@outlook.com>
2022-03-16 13:58:32 +0000

    add animation end handling

--- libs/frontend/hooks/src/lib/use-animation.ts
+++ libs/frontend/hooks/src/lib/use-animation.ts
@@ -1,42 +1,50 @@
 import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 
-export const useAnimation = (
-  duration: {
-    ms?: number;
-    s?: number;
-    m?: number;
-  },
-  loop?: boolean
-) => {
+export type AnimationDuration = Partial<{
+  ms: number;
+  s: number;
+  m: number;
+}>;
+
+export type AnimationOptions = {
+  duration: AnimationDuration;
+  loop?: boolean;
+  onEnd?: () => void;
+};
+
+export const useAnimation = ({ duration, loop, onEnd }: AnimationOptions) => {
   const animationDuration = useMemo(() => {
     const { m, ms, s } = duration ?? {};
     return (m ?? 0) * 60 * 1000 + (s ?? 0) * 1000 + (ms ?? 0);
   }, [duration]);
   const refTime = useRef(0);
   const refFrame = useRef(0);
-  const [progress, setProgress] = useState(0);
+  const [normalizedProgress, setNormalizedProgress] = useState(0);
 
   const animate = useCallback(
     (time: number) => {
       if (!animationDuration) return;
 
       refTime.current = refTime.current || time;
-      const animationProgress = time - refTime.current / animationDuration;
+      const animationProgress = (time - refTime.current) / animationDuration;
 
       if (!loop && animationProgress > 1) {
-        setProgress(() => Math.max(0, Math.min(1, animationProgress)));
+        setNormalizedProgress(() =>
+          Math.max(0, Math.min(1, animationProgress))
+        );
+        onEnd && onEnd();
         return;
       }
 
-      setProgress(() => animationProgress % 1);
+      setNormalizedProgress(() => animationProgress % 1);
       refFrame.current = requestAnimationFrame(animate);
     },
-    [animationDuration, loop]
+    [animationDuration, loop, onEnd]
   );
 
   useEffect(() => {
     refFrame.current = requestAnimationFrame(animate);
   }, [animate]);
 
-  return progress;
+  return normalizedProgress;
 };
--- libs/frontend/routes/src/lib/components/public.tsx
+++ libs/frontend/routes/src/lib/components/public.tsx
@@ -1,10 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
-import { useAnimation } from '@blubberfish/frontend/hooks';
-
 export const PublicRoutes = () => {
-  const a = useAnimation({ m: 1 });
-  console.log(a);
-
   return (
     <Routes>
       <Route index element={<div>PUBLIC ROUTE</div>} />
