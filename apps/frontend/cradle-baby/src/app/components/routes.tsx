a757e7b221d7bf49a0d11a470313e11e50d70227
Chua Siang Poh <siangpoh.chua@contour.network>
2022-03-28 17:33:52 +0800

    update route

--- apps/frontend/cradle-baby/src/app/components/routes.tsx
+++ apps/frontend/cradle-baby/src/app/components/routes.tsx
@@ -2,14 +2,15 @@ import { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { PATH } from '@blubberfish/frontend/pages/routes';
 
-const HomePage = lazy(() => import('./pages/login'));
+const LoginPage = lazy(() => import('./pages/login'));
 
 const Empty = () => null;
 
 export default () => (
   <Suspense fallback={<Empty />}>
     <Routes>
-      <Route path={PATH.ALL} element={<HomePage />} />
+      <Route path={PATH.PUBLIC.LOGIN} element={<LoginPage />} />
+      <Route path={PATH.ALL} element={<Empty />} />
     </Routes>
   </Suspense>
 );
