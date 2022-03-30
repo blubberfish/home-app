fcef2ecdba67346736c2724076b2ec2725f033fa
poh <siangpoh@outlook.com>
2022-03-30 12:29:40 +0000

    add baby service app

---
+++ apps/services/baby/src/main.ts
@@ -0,0 +1,30 @@
+import {
+  hanlderRegistrar,
+  Handler,
+  HttpHeader,
+  HttpMethod,
+} from '@blubberfish/services/core';
+
+exports.handler = async (event, context) => {
+  const { httpMethod, headers } = event;
+  if (httpMethod && hanlderRegistrar[httpMethod as HttpMethod]) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    for (const handler of Object.values<Handler<any, any, any>>(
+      hanlderRegistrar[httpMethod]
+    )) {
+      const result = await handler(event, context);
+      if (result) {
+        return {
+          ...result,
+          headers: {
+            ...result.headers,
+            'Access-Control-Allow-Credentials': 'true',
+            'Access-Control-Allow-Headers': `${HttpHeader.APIKEY}`,
+            'Access-Control-Allow-Origin': headers[HttpHeader.ORIGIN] || '*',
+          },
+        };
+      }
+    }
+  }
+  return { statusCode: 404 };
+};
