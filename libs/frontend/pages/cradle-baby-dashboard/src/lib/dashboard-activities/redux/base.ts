96037f9fac4a743a61cada59f872292ae910a5bd
poh <siangpoh@outlook.com>
2022-03-31 12:39:54 +0000

    add entity state

--- libs/frontend/pages/cradle-baby-dashboard/src/lib/dashboard-activities/redux/base.ts
+++ libs/frontend/pages/cradle-baby-dashboard/src/lib/dashboard-activities/redux/base.ts
@@ -1,9 +1,19 @@
+import { BabyActivityProfilePayload } from '@blubberfish/types';
+import { createEntityAdapter, EntityState } from '@reduxjs/toolkit';
+
 export const name = 'dashboard.activities';
+
+export const activityEntity = createEntityAdapter<BabyActivityProfilePayload>({
+  selectId: (model) => model._id,
+  sortComparer: (e1, e2) => e1.timestamp.localeCompare(e2.timestamp),
+});
 
 export type DashboardActivitiesState = {
   baby?: string | null;
+  activities: EntityState<BabyActivityProfilePayload>;
 };
 
 export const getInitialState = (): DashboardActivitiesState => ({
   baby: null,
+  activities: activityEntity.getInitialState(),
 });
