b1a7711c1a4c097ad0d7fc8e1a2d90f949415ded
poh <siangpoh@outlook.com>
2022-03-31 00:51:53 +0000

    add activity list

---
+++ apps/services/baby/src/app/get-all-activities.ts
@@ -0,0 +1,46 @@
+import {
+  ServiceException,
+  apiExceptionFactory,
+  HttpMethod,
+  registerHandler,
+  accountBabyLogCollectionFactory,
+} from '@blubberfish/services/core';
+import { BabyActivityPayload } from '@blubberfish/types';
+
+const getAllActivityHandler = async (event) => {
+  const { path, body } = event;
+  if (!/^\/?baby\/log$/i.test(path)) return null;
+
+  try {
+    const { account, baby } = (JSON.parse(body) ?? {}) as BabyActivityPayload;
+    const logs = await accountBabyLogCollectionFactory(account);
+    if (logs && baby) {
+      const activities = await logs
+        .find(
+          {
+            belongsTo: baby,
+          },
+          {
+            projection: {
+              _id: 0,
+            },
+          }
+        )
+        .sort({
+          dtob: 1,
+        });
+      return {
+        statusCode: 200,
+        body: JSON.stringify(activities),
+      };
+    }
+    return {
+      statusCode: 400,
+    };
+  } catch (e) {
+    console.error(e);
+    return apiExceptionFactory(ServiceException.Unknown, e.message, 500);
+  }
+};
+
+registerHandler(HttpMethod.GET, getAllActivityHandler);
