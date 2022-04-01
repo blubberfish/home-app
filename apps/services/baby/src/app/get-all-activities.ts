56d5750c25c60b3024b6f2789ca1e1c30aa3c8b6
poh <siangpoh@outlook.com>
2022-04-01 00:45:12 +0000

    fix api

--- apps/services/baby/src/app/get-all-activities.ts
+++ apps/services/baby/src/app/get-all-activities.ts
@@ -8,25 +8,28 @@ import {
 import { BabyActivityPayload } from '@blubberfish/types';
 
 const getAllActivityHandler = async (event) => {
-  const { path, body } = event;
+  const { path, queryStringParameters } = event;
   if (!/^\/?baby\/log$/i.test(path)) return null;
 
   try {
-    const { account, baby } = (JSON.parse(body) ?? {}) as BabyActivityPayload;
-    const logs = await accountBabyLogCollectionFactory(account);
-    if (logs && baby) {
-      const activities = await logs
-        .find({
-          belongsTo: baby,
-        })
-        .sort({
-          dtob: 1,
-        })
-        .toArray();
-      return {
-        statusCode: 200,
-        body: JSON.stringify(activities),
-      };
+    const { account, baby } = (queryStringParameters ??
+      {}) as BabyActivityPayload;
+    if (account && baby) {
+      const logs = await accountBabyLogCollectionFactory(account);
+      if (logs) {
+        const activities = await logs
+          .find({
+            belongsTo: baby,
+          })
+          .sort({
+            dtob: 1,
+          })
+          .toArray();
+        return {
+          statusCode: 200,
+          body: JSON.stringify(activities),
+        };
+      }
     }
     return {
       statusCode: 400,
