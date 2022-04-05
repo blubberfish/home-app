004f67c87bae52ae6a7e17a3f881ed3ae0171b58
Chua Siang Poh <siangpoh.chua@contour.network>
2022-04-05 11:48:48 +0800

    update routing

--- libs/frontend/pages/cradle-baby/responsive-dashboard/src/lib/pages/history/history.tsx
+++ libs/frontend/pages/cradle-baby/responsive-dashboard/src/lib/pages/history/history.tsx
@@ -1,8 +1,16 @@
 import { Module } from '@blubberfish/frontend/modules/core';
+import { Routes, Route } from 'react-router-dom';
 import slice from './redux';
 import { ChildrenTable } from './history-children-table';
 
-const HistoryPage = () => <ChildrenTable />;
+const Empty = () => null;
+
+const HistoryPage = () => (
+  <Routes>
+    <Route path=":id" element={<Empty />} />
+    <Route index element={<ChildrenTable />} />
+  </Routes>
+);
 
 export default () => (
   <Module slice={slice}>
