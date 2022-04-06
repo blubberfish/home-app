d5620afb1f67d9d2ee99f899681cde6467de7b3d
poh <siangpoh@outlook.com>
2022-04-06 14:27:03 +0000

    add v1 add child form

--- libs/frontend/pages/cradle-baby-dashboard-family/src/lib/add-child/add-child.tsx
+++ libs/frontend/pages/cradle-baby-dashboard-family/src/lib/add-child/add-child.tsx
@@ -1,7 +1,8 @@
 import { Module } from '@blubberfish/frontend/modules/core';
 import slice from './redux';
+import Page from './v1'
 
-const AddChildPage = () => null;
+const AddChildPage = () => <Page />;
 
 export default () => (
   <Module slice={slice}>
