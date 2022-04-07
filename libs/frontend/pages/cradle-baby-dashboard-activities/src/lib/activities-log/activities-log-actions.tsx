6983d652908a7536c264cb006a29a9c3729d7e67
Chua Siang Poh <siangpoh.chua@contour.network>
2022-04-07 09:36:05 +0800

    add action buttons

--- libs/frontend/pages/cradle-baby-dashboard-activities/src/lib/activities-log/activities-log-actions.tsx
+++ libs/frontend/pages/cradle-baby-dashboard-activities/src/lib/activities-log/activities-log-actions.tsx
@@ -3,6 +3,7 @@ import {
   Mitten,
   SmileBeam,
   Bed,
+  X,
 } from '@blubberfish/frontend/components/icons/font-awesome';
 import { accountIdSelector } from '@blubberfish/frontend/modules/cradle-baby/app';
 import {
@@ -14,10 +15,16 @@ import {
 import {
   color,
   ColorProps,
+  font,
+  FontProps,
   grid,
   GridProps,
+  indication,
+  IndicationType,
   padding,
   PaddingProps,
+  opacity,
+  OpacityProps,
   size,
   SizeProps,
 } from '@blubberfish/style-system';
@@ -33,14 +40,66 @@ import {
   dismissPending,
 } from './redux';
 
-const Button = styled.button<ColorProps & GridProps & PaddingProps>`
+type ButtonIndicationProps = {
+  indication?: {
+    [key in IndicationType]?: ColorProps & OpacityProps;
+  };
+};
+const buttonDisabledIndication = indication<ButtonIndicationProps>(
+  IndicationType.Hover,
+  [
+    ({ indication, theme }) =>
+      color({ theme, ...indication?.[IndicationType.Disabled] }),
+    ({ indication, theme }) =>
+      opacity({ theme, ...indication?.[IndicationType.Disabled] }),
+  ]
+);
+const buttonHoverIndication = indication<ButtonIndicationProps>(
+  IndicationType.Hover,
+  [
+    ({ indication, theme }) =>
+      color({ theme, ...indication?.[IndicationType.Hover] }),
+    ({ indication, theme }) =>
+      opacity({ theme, ...indication?.[IndicationType.Hover] }),
+  ]
+);
+const Button = styled.button<
+  ButtonIndicationProps &
+    ColorProps &
+    FontProps &
+    GridProps &
+    PaddingProps &
+    SizeProps
+>`
+  color: currentColor;
   border: 0;
   border-radius: 50%;
+  outline: 0;
+  margin: 0;
+  align-content: center;
+  justify-content: center;
+  justify-items: center;
   grid-template-columns: max-content;
   grid-template-rows: repeat(2, min-content);
+  p {
+    margin: 0;
+  }
+  svg {
+    height: 1em;
+    width: 1em;
+    fill: currentColor;
+  }
   ${color}
+  ${font}
   ${grid}
   ${padding}
+  ${size}
+  ${buttonHoverIndication}
+  ${buttonDisabledIndication}
+`;
+
+const P = styled.p<FontProps>`
+  ${font}
 `;
 
 const Container = styled.div<GridProps>`
@@ -87,7 +146,7 @@ export const LogActions = () => {
   const { list, icon, label } = useActivities();
   const handleLog = useCallback(
     (activity: BabyActivityType) => {
-      if (!account || !child || !pending) return;
+      if (!account || !child) return;
       dispatch(
         setPending({
           id: activity,
@@ -122,7 +181,7 @@ export const LogActions = () => {
         })
       );
     },
-    [account, dispatch]
+    [account, child, dispatch]
   );
 
   return (
@@ -137,16 +196,55 @@ export const LogActions = () => {
         return (
           <Button
             key={activity}
-            type="button"
-            onClick={() => {
-              handleLog(activity);
+            h="96px"
+            w="96px"
+            ftSize={3}
+            gap={1}
+            bg={pending?.id === activity ? 'success' : 'background_weak'}
+            disabled={pending?.active}
+            indication={{
+              [IndicationType.Hover]: {
+                opacity: 2,
+              },
+              [IndicationType.Disabled]: {
+                opacity: 1,
+              },
             }}
+            onClick={() => {
+              if (pending?.id !== activity) handleLog(activity);
+            }}
+            type="button"
           >
             <Icon />
-            <p>{label[activity]}</p>
+            <P ftSize={2}>{label[activity]}</P>
           </Button>
         );
       })}
+      {pending && !pending.active && (
+        <Button
+          h="96px"
+          w="96px"
+          gap={1}
+          ftSize={3}
+          bg="error"
+          disabled={pending?.active}
+          indication={{
+            [IndicationType.Hover]: {
+              opacity: 2,
+            },
+            [IndicationType.Disabled]: {
+              opacity: 1,
+            },
+          }}
+          onClick={() => {
+            dispatch(dismissPending());
+          }}
+          type="button"
+        >
+          <X />
+          <P ftSize={2}>Cancel</P>
+        </Button>
+      )}
     </Container>
   );
 };
