07b92c2eeb1a9c8d3e371b9579354d0f469288eb
poh <siangpoh@outlook.com>
2022-04-09 13:45:47 +0000

    fix slice

--- libs/frontend/pages/cradle-baby-dashboard-family/src/lib/add-child/redux/slice.ts
+++ libs/frontend/pages/cradle-baby-dashboard-family/src/lib/add-child/redux/slice.ts
@@ -1,5 +1,6 @@
+import { CreatePersonEntityPayload } from '@blubberfish/types';
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { name, getInitialState, PendingAction } from './base';
+import { Alert, name, getInitialState, PendingAction } from './base';
 
 const slice = createSlice({
   name,
@@ -8,16 +9,124 @@ const slice = createSlice({
     setPending(state, { payload }: PayloadAction<PendingAction>) {
       state.pending = payload;
     },
-    resetPending(state) {
+    dismissPending(state) {
       state.pending = null;
     },
-    startAction(state) {
+    confirmPending(state) {
       if (state.pending) {
         state.pending.started = true;
       }
+    },
+    dismissAlert(state) {
+      state.alert = null;
+    },
+    setAlert(state, { payload }: PayloadAction<Alert>) {
+      state.alert = payload;
+    },
+    setDateTimeOfBirth(state, { payload }: PayloadAction<string>) {
+      if (!state.form) {
+        state.form = {};
+      }
+      state.form.dtob = payload;
+    },
+    setFamilyName(
+      state,
+      {
+        payload: { language, value },
+      }: PayloadAction<{
+        language: keyof CreatePersonEntityPayload['name'];
+        value: string;
+      }>
+    ) {
+      if (!state.form) {
+        state.form = {};
+      }
+      if (!state.form.name) {
+        state.form.name = {};
+      }
+      if (!state.form.name[language]) {
+        state.form.name[language] = {
+          family: '',
+          given: '',
+        };
+      }
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      state.form.name[language]!.family = value;
+    },
+    setGivenName(
+      state,
+      {
+        payload: { language, value },
+      }: PayloadAction<{
+        language: keyof CreatePersonEntityPayload['name'];
+        value: string;
+      }>
+    ) {
+      if (!state.form) {
+        state.form = {};
+      }
+      if (!state.form.name) {
+        state.form.name = {};
+      }
+      if (!state.form.name[language]) {
+        state.form.name[language] = {
+          family: '',
+          given: '',
+        };
+      }
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      state.form.name[language]!.given = value;
+    },
+    setPreferredName(
+      state,
+      {
+        payload: { language, value },
+      }: PayloadAction<{
+        language: keyof CreatePersonEntityPayload['name'];
+        value: string;
+      }>
+    ) {
+      if (!state.form) {
+        state.form = {};
+      }
+      if (!state.form.name) {
+        state.form.name = {};
+      }
+      if (!state.form.name[language]) {
+        state.form.name[language] = {
+          family: '',
+          given: '',
+        };
+      }
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      state.form.name[language]!.preferred = value;
+    },
+    setGenderMale(state) {
+      if (!state.form) {
+        state.form = {};
+      }
+      state.form.gender = 'm';
+    },
+    setGenderFemale(state) {
+      if (!state.form) {
+        state.form = {};
+      }
+      state.form.gender = 'f';
     },
   },
 });
 
 export default slice;
-export const { setPending, startAction, resetPending } = slice.actions;
+export const {
+  setPending,
+  confirmPending,
+  dismissPending,
+  dismissAlert,
+  setAlert,
+  setDateTimeOfBirth,
+  setFamilyName,
+  setGenderFemale,
+  setGenderMale,
+  setGivenName,
+  setPreferredName,
+} = slice.actions;
--- libs/types/src/index.ts
+++ libs/types/src/index.ts
@@ -1,3 +1,4 @@
+export * from './lib/common/languages';
 export * from './lib/account';
 export * from './lib/baby';
 export * from './lib/service';
