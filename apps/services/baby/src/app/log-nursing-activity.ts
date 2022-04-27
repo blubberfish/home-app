f9afb9cb37b00540772235e523f8cfe1b841aedb
Chua Siang Poh <siangpoh.chua@contour.network>
2022-04-27 10:34:06 +0800

    add additional activities

--- apps/services/baby/src/app/log-nursing-activity.ts
+++ apps/services/baby/src/app/log-nursing-activity.ts
@@ -35,4 +35,62 @@ const addNursingActivityHandler = async (event) => {
   }
 };
 
+const addNursingUrinationActivityHandler = async (event) => {
+  const { path, body } = event;
+  if (!/^\/?baby\/log\/nursing$/i.test(path)) return null;
+
+  try {
+    const { account, baby, additionalDetails } = (JSON.parse(body) ??
+      {}) as BabyActivityPayload;
+    const logs = await accountBabyLogCollectionFactory(account);
+    if (logs && baby) {
+      await logs.insertOne({
+        activity: 'baby:activity:nurse:urinate',
+        belongsTo: baby,
+        timestamp: new Date(),
+        notes: additionalDetails,
+      });
+      return {
+        statusCode: 200,
+      };
+    }
+    return {
+      statusCode: 400,
+    };
+  } catch (e) {
+    console.error(e);
+    return apiExceptionFactory(ServiceException.Unknown, e.message, 500);
+  }
+};
+
+const addNursingDefecationActivityHandler = async (event) => {
+  const { path, body } = event;
+  if (!/^\/?baby\/log\/nursing$/i.test(path)) return null;
+
+  try {
+    const { account, baby, additionalDetails } = (JSON.parse(body) ??
+      {}) as BabyActivityPayload;
+    const logs = await accountBabyLogCollectionFactory(account);
+    if (logs && baby) {
+      await logs.insertOne({
+        activity: 'baby:activity:nurse:defecate',
+        belongsTo: baby,
+        timestamp: new Date(),
+        notes: additionalDetails,
+      });
+      return {
+        statusCode: 200,
+      };
+    }
+    return {
+      statusCode: 400,
+    };
+  } catch (e) {
+    console.error(e);
+    return apiExceptionFactory(ServiceException.Unknown, e.message, 500);
+  }
+};
+
 registerHandler(HttpMethod.POST, addNursingActivityHandler);
+registerHandler(HttpMethod.POST, addNursingUrinationActivityHandler);
+registerHandler(HttpMethod.POST, addNursingDefecationActivityHandler);
