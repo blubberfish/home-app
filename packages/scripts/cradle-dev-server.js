3ad3481b573cabd45afc6e5714b62082f1fd5866
Poh <siangpoh@outlook.com>
2023-09-23 13:35:35 +0000

    add dev server configuration

--- packages/scripts/cradle-dev-server.js
+++ packages/scripts/cradle-dev-server.js
@@ -1,12 +1,31 @@
 #!/usr/bin/env node
 import express from 'express';
-import { log } from './utils/index.js';
+import path from 'path';
+import { importConfig, log } from './utils/index.js';
+
+log.info(`[cradle-dev-server]: Started.`);
+
+const devServerConfig = await importConfig(
+  path.resolve('dev-server.config.js'),
+  {
+    useDefault: true,
+  },
+);
 
 function startServer() {
   const port = process.env.PORT || 8000;
   const app = express();
 
-  app.use(express.static('dist'));
+  log.info(
+    devServerConfig?.staticDir
+      ? `[cradle-dev-server]: Static files located at ${path.resolve(
+          devServerConfig.staticDir,
+        )}.`
+      : `[cradle-dev-server]: No static files.`,
+  );
+  if (devServerConfig?.staticDir) {
+    app.use(express.static(devServerConfig?.staticDir || 'dist'));
+  }
 
   app.listen(port, () => {
     log.info(`[cradle-dev-server]: Listening to port ${port}.`);
